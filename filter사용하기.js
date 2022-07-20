77895012d6fc051945ca2474b59ae43c4f41b2fb
Soobin An <jejupalette@naver.com>
2022-07-20 11:49:15 +0900

    forEach_use

--- filter사용하기.js
+++ filter사용하기.js
@@ -1,23 +1,31 @@
-/*
-let numbers = [273, 25, 75, 52, 103, 32, 57, 24, 76]
-numbers = numbers
+let numbers1 = [273, 25, 75, 52, 103, 32, 57, 24, 76]
+numbers1 = numbers1
   .filter((value) => value % 2 === 1)
   .filter((value) => value <= 100)
   .filter((value) => value % 5 === 0)
 
-console.log(numbers)
-*/
+console.log(numbers1)
 
-let numbers = [273, 25, 75, 52, 103, 32, 57, 24, 76]
+let numbers2 = [273, 25, 75, 52, 103, 32, 57, 24, 76]
 function filter() {
   let filterNumber = []
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 === 1 && numbers[i] <= 100 && numbers[i] % 5 === 0) {
-      let finallNumber = numbers[i]
+  for (let i = 0; i < numbers2.length; i++) {
+    if (numbers2[i] % 2 === 1 && numbers2[i] <= 100 && numbers2[i] % 5 === 0) {
+      let finallNumber = numbers2[i]
       filterNumber.push(finallNumber)
     }
   }
   return filterNumber
 }
 
-console.log(filter(numbers))
+console.log(filter(numbers2))
+
+let numbers3 = [273, 25, 75, 52, 103, 32, 57, 24, 76]
+function filterlist() {
+  let filterNumber = []
+  numbers[i] % 2 === 1 && numbers2[i] <= 100 && numbers2[i] % 5 === 0
+    ? filterNumber.push(numbers3[i])
+    : console.log('nothing')
+}
+
+console.log(filter(numbers3))
---
+++ forEach사용하기.js
@@ -0,0 +1,10 @@
+const array = ['사과', '배', '귤', '바나나']
+console.log('# for in 반복문')
+array.forEach((value, index) => {
+  console.log(index)
+})
+
+console.log('# for of 반복문')
+array.forEach((value) => {
+  console.log(value)
+})
